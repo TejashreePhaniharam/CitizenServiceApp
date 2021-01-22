7ff24360fccad7e699a02cddca6cc1ec7957e5dd
Tejashree Paniharam <tejashreepaniharam@gmail.com>
2021-01-22 12:12:46 +0530

    create components

---
+++ src/app/add-organization/create-orphanage/create-orphanage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-orphanage',
+  templateUrl: './create-orphanage.component.html',
+  styleUrls: ['./create-orphanage.component.css']
+})
+export class CreateOrphanageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
